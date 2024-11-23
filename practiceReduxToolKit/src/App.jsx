72f11904958ec113e66de8b66b79513719fc0fad
Nimesh <nimeshkayastha067@gmail.com>
2024-11-23 15:25:51 +0545

    live

---
+++ practiceReduxToolKit/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import AdTodo from './components/AdTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className='bg-black text-blue-300 font-bold text-3xl'>React-Redux</h1>
+    <AdTodo/>
+    <Todos/>
+    </>
+  )
+}
+
+export default App
